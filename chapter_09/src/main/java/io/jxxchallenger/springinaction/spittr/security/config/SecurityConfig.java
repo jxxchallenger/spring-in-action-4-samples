1a25d7e5ea2eba6b1753275dcd085f91a6edd6ef
jxxchallenger <jxxchallenger@foxmail.com>
2020-07-19 22:39:52 +0800

    chapter 09 基于数据库表进行认证

--- chapter_09/src/main/java/io/jxxchallenger/springinaction/spittr/security/config/SecurityConfig.java
+++ chapter_09/src/main/java/io/jxxchallenger/springinaction/spittr/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package io.jxxchallenger.springinaction.spittr.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,10 +13,16 @@ import org.springframework.security.core.userdetails.User;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+    
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.inMemoryAuthentication()
+        //auth.inMemoryAuthentication()
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .withDefaultSchema()
         .withUser(User.builder().username("user").password("{noop}password").roles("USER"))
         .withUser(User.builder().username("admin").password("{noop}password").roles("ADMIN", "USER"));
     }
