80b8bb982a1b569c057a1f76ce58ccbda2048a1e
jxxchallenger <jxxchallenger@foxmail.com>
2020-07-26 12:36:55 +0800

    chapter 09 9.3.3防止CSRF

--- chapter_09/src/main/java/io/jxxchallenger/springinaction/spittr/security/config/SecurityConfig.java
+++ chapter_09/src/main/java/io/jxxchallenger/springinaction/spittr/security/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -25,6 +27,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .withDefaultSchema()
         .withUser(User.builder().username("user").password("{noop}password").roles("USER"))
         .withUser(User.builder().username("admin").password("{noop}password").roles("ADMIN", "USER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/spitter/register").authenticated()
+        .antMatchers(HttpMethod.POST, "/spittles").authenticated()
+        //.antMatchers(HttpMethod.POST, "/spittles").hasAnyRole("USER", "ADMIN")
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .and()
+        .requiresChannel()
+        .antMatchers("/spitter/register").requiresSecure()
+        .antMatchers("/").requiresInsecure();
+        //.and().csrf().disable(); //禁用CSRF防护
     }
 
 }
